f11bb0db6864e6522600577aec9e5e8521eca6f7
mpluas <mpluasgu@hotmail.com>
2017-09-12 01:46:47 -0500

    Listo

--- Veterinaria/src/main/java/com/desarrollo/controller/menuController.java
+++ Veterinaria/src/main/java/com/desarrollo/controller/menuController.java
@@ -39,7 +39,7 @@ public class menuController implements Serializable{
     public void init(){
         this.listarMenu();
         model = new DefaultMenuModel();
-        model = establecerPermisos();
+        this.establecerPermisos();
     }
     public void listarMenu(){
         try {
@@ -58,7 +58,7 @@ public class menuController implements Serializable{
     }
     
     
-    public DefaultMenuModel establecerPermisos(){
+    public void establecerPermisos(){
         Usuarios usu = (Usuarios) FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("Usuarios");
         System.out.println("id usu " + usu.getRol().getIdrol());
         DefaultMenuItem menItm = new DefaultMenuItem("Principal");
@@ -69,113 +69,108 @@ public class menuController implements Serializable{
         menItm.setProcess("@this");
         menItm.setContainerStyleClass("layout-menubar-active");
         model.addElement(menItm);
-//        for (Menu m : lista) {
-//            System.out.println("id menu " + m.getIdmenu());
-//            if (m.getTipo().equals("S") && m.getMenuPadre() == null && m.getRol().getIdrol()==usu.getRol().getIdrol()) {
-//                DefaultSubMenu firsSubMenu = new DefaultSubMenu(m.getOpcion());
-//                firsSubMenu.setIcon(m.getRutaImagen());
-//                for (Menu i : lista) {
-//                    Menu submenu = i.getMenuPadre();
-//                    
-//                    if (submenu != null) {
-//                        System.out.println("Tiene padre " + submenu.getIdmenu() + submenu.getTipo());
-//                        System.out.println("Tiene padre2 " + i.getIdmenu() + i.getTipo());
-//                        if (i.getTipo().equals("S")) {
-//                            DefaultSubMenu nSubMenu = new DefaultSubMenu(i.getOpcion());
-//                            firsSubMenu.addElement(nSubMenu);
-//                        }
-//                        if (submenu.getIdmenu()==m.getIdmenu()){
-//                            DefaultMenuItem item = new DefaultMenuItem(i.getOpcion());
-//                            item.setIcon(i.getRutaImagen());
-//                            item.setUrl(i.getAccion());
-//                            firsSubMenu.addElement(item);
-//                        }
-//                    }
-//                }
-//                model.addElement(firsSubMenu);
-//            }else{
-//                if (m.getMenuPadre() == null && m.getRol().getIdrol()==usu.getRol().getIdrol()) {
-//                    DefaultMenuItem item = new DefaultMenuItem(m.getOpcion());
-//                    item.setIcon(m.getRutaImagen());
-//                    item.setUrl(m.getAccion());
-//                    model.addElement(item);
-//                }
-//            }
-//        }
-        for (Menu opcion : lista) {
-            if (opcion.getTipo().equals("S") && opcion.getMenuPadre() == null){
-            DefaultSubMenu submenu = new DefaultSubMenu(opcion.getOpcion());
-            System.out.println(opcion.getOpcion()
-                            + "----------------------------");
-            submenu.setId("OPTH" + opcion.getIdmenu());
-            submenu.setIcon(opcion.getRutaImagen());
-            Iterator<Menu> opcionesHijas = opcion.getMenuPadreeList()
-                            .iterator();
-            while (opcionesHijas.hasNext()) {
-                Menu opcionHija = opcionesHijas.next();
-                // Solo si hay permisos para esa opcion
-                System.out.println("opciones " + opcionHija.getIdmenu());
-                if (opcionHija.getEstado().equals("A") && opcionHija.getMenuPadre() != null) {
-                        submenu.getElements().add(getMenuHija(opcionHija));
+        for (Menu m : lista) {
+            System.out.println("id menu " + m.getIdmenu());
+            if (m.getTipo().equals("S") && m.getMenuPadre() == null && m.getRol().getIdrol()==usu.getRol().getIdrol()) {
+                DefaultSubMenu firsSubMenu = new DefaultSubMenu(m.getOpcion());
+                firsSubMenu.setIcon(m.getRutaImagen());
+                for (Menu i : lista) {
+                    Menu submenu = i.getMenuPadre();
+                    if (submenu != null) {
+                        if (submenu.getIdmenu()==m.getIdmenu()){
+                            DefaultMenuItem item = new DefaultMenuItem(i.getOpcion());
+                            item.setIcon(i.getRutaImagen());
+                            item.setUrl(i.getAccion());
+        item.setPartialSubmit(true);
+        item.setProcess("@this");
+                            firsSubMenu.addElement(item);
+                        }
+                    }
+                }
+                model.addElement(firsSubMenu);
+            }else{
+                if (m.getMenuPadre() == null && m.getRol().getIdrol()==usu.getRol().getIdrol()) {
+                    DefaultMenuItem item = new DefaultMenuItem(m.getOpcion());
+                    item.setIcon(m.getRutaImagen());
+                    item.setUrl(m.getAccion());
+                    model.addElement(item);
                 }
             }
-            model.addElement(submenu);
-        }}
-        return (DefaultMenuModel) model;
-    }
-    
-	@SuppressWarnings("el-syntax")
-	private MenuElement getMenuHija(Menu opcionPadre) {
-
-		if (opcionPadre.getMenuPadreeList()== null
-				|| opcionPadre.getMenuPadreeList().size() < 1) {
-
-			DefaultMenuItem menItm = new DefaultMenuItem(
-					opcionPadre.getOpcion());
-			// menItm.setValue(opcionPadre.getOpcion());
-			menItm.setIcon(opcionPadre.getRutaImagen());
-			// menItm.setAjax(true);
-			menItm.setTitle(opcionPadre.getDescripcion());
-			// menItm.setOncomplete("showDialogProceso("+opcionPadre.getOnComplete()+");");
-
-			// menItm.getAttributes().put("cfOpcion", opcionPadre);
-
-			if (opcionPadre.getTipo().equals("S")) {
-				// menItm.setUpdate(":MAIN_TARGET");
-			} else {
-				menItm.setOnclick("pantalla.show()");
-			}
-			// menItm.setImmediate(true);
-			// menItm.setUrl(opcionPadre.getAccion());
-			menItm.setCommand("#{verticalMenu.navegarUrl('"
-					+ opcionPadre.getAccion() + "')}");
-			menItm.setPartialSubmit(true);
-			menItm.setProcess("@this");
-			/*
-			 * menItm.setProcess("@all"); menItm.setUpdate("@all");
-			 */
-			// menItm.addActionListener(this);
-			return menItm;
-
-		} else {
-
-			DefaultSubMenu menItm = new DefaultSubMenu(opcionPadre.getOpcion());
-			menItm.setIcon(opcionPadre.getRutaImagen());
-			// menItm.setLabel(opcionPadre.getOpcion());
-
-			Iterator<Menu> opcionesHijas = opcionPadre.getMenuPadreeList()
-					.iterator();
-
-			while (opcionesHijas.hasNext()) {
-				Menu opcionHija = opcionesHijas.next();
-				// Solo voy a recorrer si hay permisos para esa opcion
-                                    if (opcionHija.getMenuPadre() != null) {
-                                menItm.getElements().add(getMenuHija(opcionHija));
-                            }
-					
-			}
-			return menItm;
-		}
-
-	}
+        }
+//        for (Menu opcion : lista) {
+//            if (opcion.getTipo().equals("S") && opcion.getMenuPadre() == null){
+//            DefaultSubMenu submenu = new DefaultSubMenu(opcion.getOpcion());
+//            System.out.println(opcion.getOpcion()
+//                            + "----------------------------");
+//            //submenu.setId("OPTH" + opcion.getIdmenu());
+//            submenu.setIcon(opcion.getRutaImagen());
+//            Iterator<Menu> opcionesHijas = opcion.getMenuPadreeList()
+//                            .iterator();
+//            while (opcionesHijas.hasNext()) {
+//                Menu opcionHija = opcionesHijas.next();
+//                // Solo si hay permisos para esa opcion
+//                System.out.println("opciones " + opcionHija.getIdmenu());
+//                if (opcionHija.getEstado().equals("A") && opcionHija.getMenuPadre() != null) {
+//                        submenu.getElements().add(getMenuHija(opcionHija));
+//                }
+//            }
+//            model.addElement(submenu);
+//        }}
+//        return (DefaultMenuModel) model;
+//    }
+//    
+//	@SuppressWarnings("el-syntax")
+//	private MenuElement getMenuHija(Menu opcionPadre) {
+//
+//		if (opcionPadre.getMenuPadreeList()== null
+//				|| opcionPadre.getMenuPadreeList().size() < 1) {
+//
+//			DefaultMenuItem menItm = new DefaultMenuItem(opcionPadre.getOpcion());
+//			// menItm.setValue(opcionPadre.getOpcion());
+//			menItm.setIcon(opcionPadre.getRutaImagen());
+//			// menItm.setAjax(true);
+//			menItm.setTitle(opcionPadre.getDescripcion());
+//                        menItm.setUrl(opcionPadre.getAccion());
+//			// menItm.setOncomplete("showDialogProceso("+opcionPadre.getOnComplete()+");");
+//
+//			// menItm.getAttributes().put("cfOpcion", opcionPadre);
+//
+////			if (opcionPadre.getTipo().equals("S")) {
+////				// menItm.setUpdate(":MAIN_TARGET");
+////			} else {
+////				menItm.setOnclick("pantalla.show()");
+////			}
+//			// menItm.setImmediate(true);
+//			// menItm.setUrl(opcionPadre.getAccion());
+////			menItm.setCommand("#{verticalMenu.navegarUrl('"
+////					+ opcionPadre.getAccion() + "')}");
+//			//menItm.setPartialSubmit(true);
+//			//menItm.setProcess("@this");
+//			/*
+//			 * menItm.setProcess("@all"); menItm.setUpdate("@all");
+//			 */
+//			// menItm.addActionListener(this);
+//			return menItm;
+//
+//		} else {
+//
+//			DefaultSubMenu menItm = new DefaultSubMenu(opcionPadre.getOpcion());
+//			menItm.setIcon(opcionPadre.getRutaImagen());
+//			// menItm.setLabel(opcionPadre.getOpcion());
+//
+//			Iterator<Menu> opcionesHijas = opcionPadre.getMenuPadreeList()
+//					.iterator();
+//
+//			while (opcionesHijas.hasNext()) {
+//				Menu opcionHija = opcionesHijas.next();
+//				// Solo voy a recorrer si hay permisos para esa opcion
+//                                    if (opcionHija.getMenuPadre() != null) {
+//                                menItm.getElements().add(getMenuHija(opcionHija));
+//                            }
+//					
+//			}
+//			return menItm;
+//		}
+//
+}
 }
